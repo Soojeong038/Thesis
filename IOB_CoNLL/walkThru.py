5b161ceb05ef4a9989fba42592741f10d026fe1a
Soojeong <97410949+Soojeong038@users.noreply.github.com>
2022-09-04 17:38:21 +0200

    Create walkThru.py

---
+++ IOB_CoNLL/walkThru.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import os
+import glob
+
+
+# In[4]:
+
+
+#get leaf directories
+def leaf_folders(file_path):
+    return [dirpaths for dirpaths, dirnames, filenames in os.walk(file_path) if not dirnames]
+
+
+#get files
+def files(file_path,extension):
+    folder_list = leaf_folders(file_path)
+    files_list = {}
+    
+    for i in folder_list:
+#         time = os.path.basename(i)
+        os.chdir(i)
+        files = []
+        for file in glob.glob("*.{}".format(extension)):
+            files.append(file)
+        
+        files_list.update({i:files})
+        
+    return files_list
+
+
+# In[5]:
+
+
+if __name__ == '__main__':
+    path = "C:\\Users\\sooje\\daten"
+    files_list = files(path,'pdf')
+    print(files_list)
